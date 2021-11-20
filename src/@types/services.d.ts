ff1c8ce99d44443d2fce79724e624ee976fc84df
Eduardo Pinheiro <3dup1n@gmail.com>
2021-11-20 17:18:41 -0300

    minor fixes

--- src/@types/services.d.ts
+++ src/@types/services.d.ts
@@ -1,24 +1,3 @@
-type TPost = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-};
-
-type TComment = {
-  postId: number;
-  id: number;
-  name: string;
-  email: string;
-  body: string;
-};
-
-type TPostWithLikes = { likes: number } & TPost;
-
-type TGetPostResponse = {
-  data: TPost[];
-};
-
-type TGetPostCommentsResponse = {
-  data: TComment[];
+type THello = {
+  hello: string;
 };
